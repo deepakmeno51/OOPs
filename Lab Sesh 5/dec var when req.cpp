a92475ebfece3ba13c578c4b7198e2d0ad28ca25
deepakmeno51 <deepakmeno51@gmail.com>
2025-02-17 16:46:35 +0800

    oops

---
+++ Lab Sesh 5/dec var when req.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Enter a float number: ";
+    float deepakFloat;
+    cin >> deepakFloat;
+    cout << "You entered: " << deepakFloat << endl;
+
+    cout << "Enter a number: ";
+    double deepakDouble;
+    cin >> deepakDouble;
+    cout << "You entered: " << deepakDouble << endl;
+
+    cout << "Enter a character: ";
+    char deepakChar;
+    cin >> deepakChar;
+    cout << "You entered: " << deepakChar << endl;
+
+    return 0;
+}
